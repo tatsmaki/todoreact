447601b51967dd912f9cc366714ee8cc3c9afdb2
Rastsislau Kharlanau <tatsmaki@yahoo.com>
2021-02-25 15:47:13 +0300

    fix: smt of eslint

--- todo/.eslintrc.js
+++ todo/.eslintrc.js
@@ -1,12 +1,10 @@
 module.exports = {
   extends: ['airbnb-typescript'],
   parserOptions: {
-    files: ['*.ts', '*.tsx'],
-    project: ['./tsconfig.json'],
+    project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: "module",
   },
-  parser: '@typescript-eslint/parser',
   rules: {
     "react/jsx-props-no-spreading": "off",
     "react/state-in-constructor": 0,
--- todo/src/components/elements/Textarea/index.ts
+++ todo/src/components/elements/Textarea/index.ts
@@ -1,3 +1,3 @@
-import TextArea from './TextArea';
+import TextArea from './Textarea';
 
 export default TextArea;
